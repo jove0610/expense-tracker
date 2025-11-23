988e7294a523b8052419e50c5fdbbe7bec6ad6bb
jove0610 <jove0610@gmail.com>
2025-11-24 04:43:13 +0800

    add category filter

--- src/daily-expense/components/DailyExpense.tsx
+++ src/daily-expense/components/DailyExpense.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import dayjs from "dayjs";
 import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { type SelectChangeEvent } from "@mui/material/Select";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 
@@ -8,13 +12,18 @@ import AddExpenseModal from "./AddExpenseModal";
 import ExpenseBreakdown from "./ExpenseBreakdown";
 import { formatToPeso } from "../utilities";
 import { useExpenses } from "../hooks";
+import { MOCK_CATEGORY } from "../mocks";
 
 function DailyExpense() {
   const [showAddModal, setShowAddModal] = useState(false);
+  const [categoryId, setCategoryId] = useState("");
   const [expenses, setExpenses] = useExpenses();
 
+  const now = dayjs();
   const expensesForThisMonth = expenses.filter((expense) => {
-    return dayjs(expense.date).isSame(dayjs(), "month");
+    const isThisMonth = dayjs(expense.date).isSame(now, "month");
+    const matchesCategory = !categoryId || expense.categoryId === categoryId;
+    return isThisMonth && matchesCategory;
   });
   const totalExpense = expensesForThisMonth.reduce((acc, curr) => {
     return acc + curr.amount;
@@ -22,6 +31,15 @@ function DailyExpense() {
 
   const toggleAddItemModal = () => {
     setShowAddModal((prev) => !prev);
+  };
+
+  const handleCategoryFilter = (e: SelectChangeEvent<string>) => {
+    e.preventDefault();
+    setCategoryId(e.target.value);
+  };
+
+  const handleClearFilters = () => {
+    setCategoryId("");
   };
 
   return (
@@ -33,9 +51,37 @@ function DailyExpense() {
         <Typography component="h3">{dayjs().format("MMMM")} Expense</Typography>
         <Typography variant="h4">{formatToPeso(totalExpense)}</Typography>
       </Stack>
-      <Button variant="outlined" onClick={toggleAddItemModal}>
+      <Button variant="outlined" sx={{ mb: 5 }} onClick={toggleAddItemModal}>
         Add Expense
       </Button>
+
+      <FormControl>
+        <InputLabel id="category-filter" size="small">
+          Filter By Category
+        </InputLabel>
+        <Select
+          id="category-filter"
+          label="Filter By Category"
+          name="category"
+          size="small"
+          value={categoryId}
+          onChange={handleCategoryFilter}
+        >
+          {MOCK_CATEGORY.map((category) => (
+            <MenuItem key={category.id} value={category.id}>
+              {category.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <Button
+        variant="outlined"
+        sx={{ mr: "auto" }}
+        onClick={handleClearFilters}
+      >
+        Clear Filters
+      </Button>
+
       <ExpenseBreakdown expenses={expensesForThisMonth} />
 
       <AddExpenseModal
